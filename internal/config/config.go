51acf5e77045409276cff7c084e0b788a47a2a38
Sergey Malinovskiy <malinovskiy@firecode-team.ru>
2024-11-18 17:54:54 +0300

    Updated storage config

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,12 +10,21 @@ import (
 type Config struct {
 	Env        string `yaml:"env" env-default:"local"`
 	HTTPServer `yaml:"http_server"`
+	Storage    `yaml:"database"`
 }
 
 type HTTPServer struct {
 	Address     string        `yaml:"address" env-default:"localhost:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+type Storage struct {
+	Host     string `yaml:"host" env-default:"localhost"`
+	Port     string `yaml:"port" env-default:"5432"`
+	DbName   string `yaml:"db_name"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
 }
 
 func MustLoad() *Config {
